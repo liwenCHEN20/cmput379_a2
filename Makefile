42566a983a7e2bf984e57c551d2dd57861c60e8c
Corey Jamison <cjamison@ualberta.ca>
2014-11-05 20:44:38 -0800

    server_p now serves a goodbye, world page and receives the get request from the client

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 CC=gcc
-CFLAGS=-m32
+CFLAGS=-m32 -g
+LDFLAGS=-m32
 DEPS=server.h
 
 all:	server_p server_f
 
 server_f:	main.o server_f.o
+	$(CC) -o $@ main.o server_f.o $(LDFLAGS)
 
 server_p:	main.o server_p.o
+	$(CC) -o $@ main.o server_p.o $(LDFLAGS)
 
 %.o:	%.c $(DEPS)
 	$(CC) -c -o $@ $< $(CFLAGS) 
--- server_p.c
+++ server_p.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
@@ -10,27 +13,54 @@ char* appName = "server_p";
 void runServer(int port, char* docDir, char* logDir)
 {
 	printf("port: %i\ndocDir: %s\nlogDir: %s\n", port, docDir, logDir);
-	int socketId = socket(AF_INET, SOCK_STREAM, 0);
-	
-	struct sockaddr_in address;
-	address.sin_family = AF_INET;
-	address.sin_port = port;
-	address.sin_addr = (struct in_addr) INADDR_LOOPBACK;
 
-	if(bind(socketId, (struct sockaddr*) &address, sizeof(sockaddr_in)) == -1)
-	{
-		/* error */
+	int sock, snew, fromlength, number, outnum;
+
+	struct sockaddr_in master, from;
+
+	sock = socket( AF_INET, SOCK_STREAM, 0);
+	if( sock < 0 ) {
+		perror( "Server: cannot open master socket");
+		exit(1);
 	}
-	else
-	{
-		if(listen(socketId, 50) == -1)
-		{
-			/* error */
+
+	master.sin_family = AF_INET;
+	master.sin_addr.s_addr = INADDR_ANY;
+	master.sin_port = htons(port);
+
+	if(bind(sock, (struct sockaddr*) &master, sizeof(master))) {
+		perror( "Server: cannot bind master socket");
+		exit(1);
+	}
+
+	number = 0;
+	listen(sock, 16);
+
+	while(1) {
+		fromlength = sizeof(from);
+		snew = accept(sock, (struct sockaddr*) &from, &fromlength);
+		if(snew < 0) {
+			perror("Server: accept failed");
+			exit(1);
 		}
-		else
-		{
-			
-		}
+
+		char request[512];
+
+		read(snew, request, sizeof(request) - 1);
+
+		printf("Request: \n%s\n\n", request);
+
+		char response[] = "HTTP/1.1 200 OK\r\n"
+"Content-Type: text/html; charset=UTF-8\r\n\r\n"
+"<doctype !html><html><head><title>Bye-bye baby bye-bye</title>"
+"<style>body { background-color: #111 }"
+"h1 { font-size:4cm; text-align: center; color: black;"
+" text-shadow: 0 0 2mm red}</style></head>"
+"<body><h1>Goodbye, world!</h1></body></html>\r\n";
+
+		write(snew, response, sizeof(response) - 1);
+		close(snew);
+		
 	}
 
 }
