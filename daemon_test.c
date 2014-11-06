8bff01cc5e9506ab2bfbf3b812c45afe60b1ed9f
Corey Jamison <cjamison@ualberta.ca>
2014-11-05 21:00:30 -0800

    Now serves a 400 malformed request page formatted as per the pdf

--- daemon_test.c
+++
@@ -1,5 +0,0 @@
-#include <unistd.h>
-int main(int argc, char* args[]) {
-	daemon(0, 0);
-	sleep(30);
-}
--- server_p.c
+++ server_p.c
@@ -50,15 +50,31 @@ void runServer(int port, char* docDir, char* logDir)
 
 		printf("Request: \n%s\n\n", request);
 
-		char response[] = "HTTP/1.1 200 OK\r\n"
+//		char response[] = "HTTP/1.1 200 OK\r\n"
+//"Content-Type: text/html; charset=UTF-8\r\n\r\n"
+//"<doctype !html><html><head><title>Bye-bye baby bye-bye</title>"
+//"<style>body { background-color: #111 }"
+//"h1 { font-size:4cm; text-align: center; color: black;"
+//" text-shadow: 0 0 2mm red}</style></head>"
+//"<body><h1>Goodbye, world!</h1></body></html>\r\n";
+		char response_400[] = "HTTP/1.1 400 Bad Request\r\n"
+"Date: Mon 21 Jan 2008 18:06:16 GMT\r\n"
 "Content-Type: text/html; charset=UTF-8\r\n\r\n"
-"<doctype !html><html><head><title>Bye-bye baby bye-bye</title>"
-"<style>body { background-color: #111 }"
-"h1 { font-size:4cm; text-align: center; color: black;"
-" text-shadow: 0 0 2mm red}</style></head>"
-"<body><h1>Goodbye, world!</h1></body></html>\r\n";
+//"Content-Length: 107\r\n\r\n"
+"<doctype !html><html><head><title>Test 123</title><body>"
+"<h2>Malformed Request</h2>"
+"Your browser sent a request I could not understand."
+"</body></html>\r\n";
 
-		write(snew, response, sizeof(response) - 1);
+		char response_400_frompdf[] = "HTTP/1.1 400 Bad Request\r\n"
+"Date: Mon 21 Jan 2008 18:06:16 GMT\r\n"
+"Content-Type: text/html\r\n"
+"Content-Length: 107\r\n\r\n"
+"<html><body>\r\n"
+"<h2>Malformed Request</h2>\r\n"
+"Your browser sent a request I could not understand.\r\n"
+"<body></html>\r\n";
+		write(snew, response_400_frompdf, sizeof(response_400_frompdf) - 1);
 		close(snew);
 		
 	}
